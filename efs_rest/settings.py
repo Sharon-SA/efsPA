a4c2560abef1eff64c5b9541aadb27f16e90089b
Sharon-SA <ssotunde-adesina@unomaha.edu>
2022-10-30 23:48:00 -0500

    ic

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
+STATICFILES_DIRS = [ 
+    os.path.join(BASE_DIR, 'static')
+],
 
 DEBUG = True
 REST_FRAMEWORK = {
