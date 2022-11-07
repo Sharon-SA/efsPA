252a148f1550b1ce4b7014fd720d1840a2059d8a
Sharon-SA <ssotunde-adesina@unomaha.edu>
2022-11-07 12:24:58 -0600

    f

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -125,11 +125,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, "static")
-]
+#STATICFILES_DIRS = [ 
+    #os.path.join(BASE_DIR, "static")
+#]
 DEBUG = True
 
 REST_FRAMEWORK = {
