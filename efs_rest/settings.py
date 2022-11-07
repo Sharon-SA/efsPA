de32e102fcb999c368cfdcbca46efb62d93aff7d
Sharon-SA <ssotunde-adesina@unomaha.edu>
2022-11-07 12:48:43 -0600

    n

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
+#STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 DEBUG = True
